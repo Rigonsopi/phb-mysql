d98106daf0fba51d2f9b7d84020aa04322a3857f
Rigonsopi <rigonsopi5@gmail.com>
2025-01-25 15:41:51 +0000

    first php file

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+//$school = "Digital School";
+//$age = 15;
+
+//echo "I love $school"; permes $.. e thirr fjalin pa e shkru hiq
+//echo "My age is $age";
+
+
+//$x = 120;
+//$y = 20;
+
+//echo $x + $y //munet me kan qfardo veprimi matematikor
+
+
+$name = "Rigon";
+$surname = "Sopi";
+
+$fullName = $name . " " . $surname; //permes ksaj i bashkon te dyjat
+
+echo $fullName;
+echo strlen($name); //numron sa e gat o fjala ne name
+echo str_word_count($name); //tregon sa fjal jon ne name
+echo strrev($name); //e kthen fjalen mbrapsht
+
+
+
+
+
+?>
