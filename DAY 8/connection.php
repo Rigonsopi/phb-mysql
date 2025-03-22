fbd58e77bbaad23762ef4d7307db37ab3a9cd359
Rigonsopi <rigonsopi5@gmail.com>
2025-03-22 15:16:28 +0000

    ..

--- DAY 8/connection.php
+++ DAY 8/connection.php
@@ -1,18 +1,26 @@
 <?php
 
 try{
-    $pdo = new PDO("mysql:host=localhost;dbname=database", "root", "");
+    $pdo = new PDO("mysql:host=localhost;dbname=rigonii", "root", "");
 
-    $username = "Rigoni";
-    $password = password_hash("mypassword", PASSWORD_DEFAULT);
+   // $username = "Rigon";
+   // $password = password_hash("itsmypass", PASSWORD_DEFAULT);
 
-    $sql = "INSERT INTO user(username,password) VALUES ("$username", "$password")";
+   // $sql = "INSERT INTO rigonii(username,password) VALUES ('$username', '$password')";
 
+   // $sql = "ALTER table rigonii ADD email VARCHAR(255)"; //komand me te cilen mujna me shtu diqka prsh kolon ne nje tabel ekzistuese
+  
+   // $sql = "ALTER table rigonii DROP COLUMN email"; //komand per me hjek ni kolon prej ni tabele ekzistuese
+    
+   // $sql = "DROP TABLE PRODUCTS"; //komand e cila e fshin komplet ni tabel, e njejta munet me u bo edhe me database
+    
     $pdo->exec($sql);
 
     echo "New record created successfully";
 }catch(DOExecption $e){
     echo $e->getMessage();
 }
+
+
 
 ?>
