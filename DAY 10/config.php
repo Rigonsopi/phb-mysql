4c7c58cbc0161fa6daf4f2de2cf60e73693f287e
Rigonsopi <rigonsopi5@gmail.com>
2025-05-10 14:48:59 +0000

    ..

--- DAY 10/config.php
+++ DAY 10/config.php
@@ -4,7 +4,7 @@
     $server = 'localhost';
     $username = 'root';
     $password = '';
-    $dbname = 'users';
+    $dbname = 'dbtest';
 
     try{
         $connect = new PDO("mysql:host=$server;dbname=$dbname", $username, $password);
