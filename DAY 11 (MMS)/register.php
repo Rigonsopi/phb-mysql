9ee303022b93f2a6361ee92f51934a5306706eda
Rigonsopi <rigonsopi5@gmail.com>
2025-06-21 14:41:52 +0000

    ..

--- DAY 11 (MMS)/register.php
+++ DAY 11 (MMS)/register.php
@@ -1,32 +1,34 @@
 <?php
-    include_once('config.php');
+include_once('config.php');
 
-    if(isset($_POST['submit'])){
-        $emri = $_POST['emri'];
-        $username = $_POST['username'];
-        $email = $_POST['email'];
+if(isset($_POST['submit'])){
+    $emri = $_POST['emri'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
 
-        $tempPass = $_POST['password'];
-        $password = password_hash($tempPass, PASSWORD_DEFAULT);
+    $tempPass = $_POST['password'];
+    $password = password_hash($tempPass, PASSWORD_DEFAULT);
 
-        $tempConfirm = $_POST['confirm_password'];
-        $confirm_password = password_hash($tempConfirm, PASSWORD_DEFAULT);
+    $tempConfirm = $_POST['confirm_password'];
+    $confirmPassword = password_hash($tempConfirm, PASSWORD_DEFAULT);
 
-        if(empty($emri) || empty($username) || empty($email) || empty($password) || empty($confirm_password)) {
-            echo "You have not filled in all the fields.";
-        }
-        else{
-            $sql = "INSERT INTO users (emri, username, email, password, confirm_password) values (:emri, :username, :email, :password, :confirm_password)";
+    if(empty($emri) || empty($username) || empty($email) || empty($password) || empty($confirmPassword)){
+        echo "You have not filled all the fields.";
+}
+else{
+    $sql = "INSERT INTO users (emri, username, email, password, confirm_password) values (:emri, :username, :email, :password, :confirm_password)";
 
-            $insertsql = $conn->bindeParam(':emri', $emri);
-            $insertsql = $conn->bindeParam(':username', $username);
-            $insertsql = $conn->bindeParam(':email', $email);
-            $insertsql = $conn->bindeParam(':password', $password);
-            $insertsql = $conn->bindeParam(':confirm_password', $confirm_password);
+    $insertsql = $conn->prepare($sql);
 
-            $insertsql->execute();
+    $insertsql->bindParam(':emri' , $emri);
+    $insertsql->bindParam(':username' , $username);
+    $insertsql->bindParam(':email' , $email);
+    $insertsql->bindParam(':password' , $password);
+    $insertsql->bindParam(':confirm_password' , $confirmPassword);
 
-            header("Location: login.php");
-        }
-    }
+    $insertsql->execute();
+
+    header("Location: login.php");
+}
+}
 ?>
